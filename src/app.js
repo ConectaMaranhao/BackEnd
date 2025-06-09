e8f2d42841718b09bef1c00cf323e15bbba48799
Ytalo Santos <ytalos762@gmail.com>
2025-06-09 18:15:42 -0300

    setting db connection

--- src/app.js
+++ src/app.js
@@ -1,7 +1,21 @@
 import express from "express";
 import routes from "./routes/index.js";
+import databaseConnector from "./config/dbConnection.js";
+import jsonParser from "./middlewares/typeParser.js";
+
+const connection = await databaseConnector();
+
+connection.on("error", (error) => {
+    console.error(`Connection error: ${error}`);
+});
+
+connection.once("open", () => {
+    console.log("Connected with database.");
+});
 
 const app = express();
+app.use(jsonParser);
+
 routes(app);
 
 export default app;
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,6 @@ import express from "express";
 
 const routes = (app) => {
     app.route("/").get((req, res) => res.status(200).send("Server on."));
-    //app.use(express.json());
 }
 
 export default routes;
