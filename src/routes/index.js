83b5d2fa03fa478de5a88e7224f869cb19106cec
Ytalo Santos <ytalos762@gmail.com>
2025-06-13 17:44:09 -0300

    fixing routes import

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 import express from "express";
+import users from "./userRoutes.js";
 
 const routes = (app) => {
     app.route("/").get((req, res) => res.status(200).send("Server on."));
+    app.use(users);
 }
 
 export default routes;
---
+++ src/routes/userRoutes.js
@@ -0,0 +1,9 @@
+import express from "express";
+import UserController from "../controllers/UserController.js";
+
+const router = express.Router();
+
+router.get("/users", UserController.getAll);
+router.post("/users", UserController.createUser);
+
+export default router;
