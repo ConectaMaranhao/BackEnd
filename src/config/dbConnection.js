6de7ecbcc0aa9c1e10f6f21564867357b364a621
Ytalo Santos <ytalos762@gmail.com>
2025-06-14 11:04:42 -0300

    setting database config

--- src/config/dbConnection.js
+++ src/config/dbConnection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import "dotenv/config";
 
 async function databaseConnector() {
-    mongoose.connect(process.env.DB_CONNECTION_STRING);
+    mongoose.connect(process.env.MONGO_DB_ATLAS_CONNECTION_STRING);
     return mongoose.connection;
 }
 
