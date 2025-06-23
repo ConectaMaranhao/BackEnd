23fd6e5f567f2ea12601eec880ec6032dc6fdaa7
Ytalo Santos <ytalos762@gmail.com>
2025-06-23 12:10:30 -0300

    criando model para matricula

---
+++ src/models/Matricula.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const matriculaSchema = new mongoose.Schema({
+    pessoaId: { 
+        type: mongoose.Types.ObjectId,
+        ref: "pessoa",
+        required: true
+    },
+    cursoId: {
+        type: mongoose.Types.ObjectId,
+        ref: "curso",
+        required: true
+    },
+    dataMatricula: {
+        type: Date,
+        default: Date.now,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ["ativa", "cancelada"],
+        default: "ativa",
+        required: true
+    }
+});
+
+const matriculaModel = mongoose.model("matricula", matriculaSchema);
+
+export default matriculaModel;
