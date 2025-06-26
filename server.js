ff06b3750f655ea19def01fad0f78a04d0c0be96
Ytalo Santos <ytalos762@gmail.com>
2025-06-26 18:48:10 -0300

    removendo log de teste

--- server.js
+++ server.js
@@ -5,6 +5,4 @@ const PORT = 4000;
 
 app.listen(PORT, () => {
     console.log(`Server running on: https://conectamaranhao.onrender.com`);
-    const JWT_SECRET = process.env.JWT_SECRET;
-    console.log(JWT_SECRET);
 });
